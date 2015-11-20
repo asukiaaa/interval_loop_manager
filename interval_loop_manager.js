3a256dd4d1c713fbb2db40dffea78f12f2d8665d
asuki <asukiaaa@gmail.com>
2015-11-20 11:28:11 +0900

    Added interval_loop_manager.js.

---
+++ interval_loop_manager.js
@@ -0,0 +1,44 @@
+var intervalLoopManager = function(functionToLoop, options) {
+  this.functionToLoop = functionToLoop
+  this.assignValues(options)
+  this.start()
+}
+
+intervalLoopManager.prototype.start = function(options) {
+  this.assignValues(options)
+  if ( typeof this.interval === 'undefined' ||
+       this.isPresent() ) {
+    return
+  }
+  var that = this
+  function loop(){
+    that.functionToLoop()
+    that.timeout = setTimeout(loop, that.interval)
+  }
+  loop()
+}
+
+intervalLoopManager.prototype.stop = function() {
+  if ( this.isPresent() ) {
+    clearTimeout(this.timeout)
+    this.timeout = null
+  }
+}
+
+intervalLoopManager.prototype.restart = function(options) {
+  this.stop()
+  this.start(options)
+}
+
+intervalLoopManager.prototype.assignValues = function(options) {
+  if ( typeof options !== 'undefined' ){
+    if ( typeof options['interval'] !== 'undefined' ) {
+      this.interval = options['interval']
+    }
+  }
+}
+
+intervalLoopManager.prototype.isPresent = function(options) {
+  return ( typeof this.timeout !== 'undefined' &&
+           this.timeout != null )
+}
